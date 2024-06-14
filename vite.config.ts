b49c9153b0a811c8f88fcc64e09d9e268c59548c
Saar Tako <saartako25@gmail.com>
2024-06-14 18:33:00 +0300

    using just vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,51 +1,7 @@
-/// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  root: __dirname,
-  cacheDir: './node_modules/.vite/portfolio',
-
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  build: {
-    outDir: './dist/portfolio',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
-
-  test: {
-    watch: false,
-    globals: true,
-    cache: {
-      dir: './node_modules/.vitest/portfolio',
-    },
-    environment: 'jsdom',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: './coverage/portfolio',
-      provider: 'v8',
-    },
-  },
-});
+  plugins: [react()],
+})
