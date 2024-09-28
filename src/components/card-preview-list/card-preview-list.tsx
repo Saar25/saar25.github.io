0e9541e52a659f8bcc5b8a2474c294bd16b7924b
Saar Tako <saartako25@gmail.com>
2024-09-29 00:09:53 +0300

    added swipe js library

--- src/components/card-preview-list/card-preview-list.tsx
+++ src/components/card-preview-list/card-preview-list.tsx
@@ -1,3 +1,10 @@
+'use client';
+
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import { Autoplay, Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Card } from '../../types/card.types';
 import { CardPreview } from '../card-preview/card-preview';
 import styles from './card-preview-list.module.scss';
@@ -9,9 +16,21 @@ export interface CardPreviewListProps {
 export const CardPreviewList = ({ cards }: CardPreviewListProps) => {
     return (
         <div className={styles['card-preview-list-container']}>
-            {cards.map(card => (
-                <CardPreview card={card} key={card.id} />
-            ))}
+            <Swiper
+                className={styles['card-preview-list-swiper']}
+                modules={[Navigation, Autoplay]}
+                loop={true}
+                navigation={true}
+                centeredSlides={true}
+                autoplay={{ delay: 2500 }}
+                slidesPerView={5}
+            >
+                {cards.map(card => (
+                    <SwiperSlide className={styles['card-preview-list-swiper-slide']}>
+                        <CardPreview card={card} key={card.id} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     );
 };
