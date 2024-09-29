2b12ae5b49feed242fec4d7bdbb16f366a41ef4b
Saar Tako <saartako25@gmail.com>
2024-09-30 00:11:08 +0300

    added some skills and reorganized

--- src/hooks/card.hooks.ts
+++ src/hooks/card.hooks.ts
@@ -1,30 +1,34 @@
-import { Card } from '../types/card.types';
 import angular from '../assets/logos/angular.png';
-import nodejs from '../assets/logos/nodejs.png';
+import docker from '../assets/logos/docker.svg';
+import git from '../assets/logos/git.png';
 import java from '../assets/logos/java.png';
 import kotlin from '../assets/logos/kotlin.svg';
-import ts from '../assets/logos/ts.png';
-import sql from '../assets/logos/sql.png';
-import react from '../assets/logos/react.png';
-import git from '../assets/logos/git.png';
-import nestjs from '../assets/logos/nestjs.svg';
-import scss from '../assets/logos/scss.png';
 import lwjgl from '../assets/logos/lwjgl.svg';
-import blender from '../assets/logos/blender.svg';
+import nestjs from '../assets/logos/nestjs.svg';
+import nextjs from '../assets/logos/nextjs.svg';
+import nodejs from '../assets/logos/nodejs.png';
+import react from '../assets/logos/react.png';
+import scss from '../assets/logos/scss.png';
+import sql from '../assets/logos/sql.png';
+import ts from '../assets/logos/ts.png';
+import { Card } from '../types/card.types';
+
+const cards = [
+    { title: 'NodeJs', image: nodejs.src },
+    { title: 'NextJs', image: nextjs.src },
+    { title: 'React', image: react.src },
+    { title: 'Docker', image: docker.src },
+    { title: 'Typescript', image: ts.src },
+    { title: 'NestJs', image: nestjs.src },
+    { title: 'Angular', image: angular.src },
+    { title: 'Java', image: java.src },
+    { title: 'Kotlin', image: kotlin.src },
+    { title: 'SQL', image: sql.src },
+    { title: 'Git', image: git.src },
+    { title: 'Scss', image: scss.src },
+    { title: 'Lwjgl', image: lwjgl.src },
+].map((card, id) => ({ id, ...card }) as const) satisfies Card[];
 
 export const useCards = () => {
-    return [
-        { id: 0, title: 'Angular', image: angular.src },
-        { id: 2, title: 'NodeJs', image: nodejs.src },
-        { id: 3, title: 'Java', image: java.src },
-        { id: 4, title: 'Kotlin', image: kotlin.src },
-        { id: 5, title: 'Typescript', image: ts.src },
-        { id: 6, title: 'SQL', image: sql.src },
-        { id: 7, title: 'React', image: react.src },
-        { id: 8, title: 'Git', image: git.src },
-        { id: 9, title: 'NestJs', image: nestjs.src },
-        { id: 10, title: 'Scss', image: scss.src },
-        { id: 11, title: 'Lwjgl', image: lwjgl.src },
-        { id: 12, title: 'Blender', image: blender.src },
-    ] as const satisfies Card[];
+    return cards;
 };
