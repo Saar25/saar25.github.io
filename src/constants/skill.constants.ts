7450fd5b78cf18d224797255e9d88724e3178aaf
Saar Tako <saartako25@gmail.com>
2025-04-18 20:03:47 +0300

    added logos

--- src/constants/skill.constants.ts
+++ src/constants/skill.constants.ts
@@ -8,9 +8,15 @@ import nestjs from '../assets/logos/nestjs.svg';
 import nextjs from '../assets/logos/nextjs.svg';
 import nodejs from '../assets/logos/nodejs.png';
 import react from '../assets/logos/react.png';
-import sass from '../assets/logos/scss.png';
+import sass from '../assets/logos/sass.png';
 import sql from '../assets/logos/sql.png';
 import ts from '../assets/logos/ts.png';
+import js from '../assets/logos/js.svg';
+import cpp from '../assets/logos/cpp.svg';
+import python from '../assets/logos/python.svg';
+import html5 from '../assets/logos/html5.svg';
+import mongodb from '../assets/logos/mongodb.svg';
+import postgresql from '../assets/logos/postgresql.svg';
 
 export const skills = [
     'nodejs',
@@ -20,8 +26,7 @@ export const skills = [
     'js',
     'ts',
     'C/C++',
-    'Html5',
-    'Css5',
+    'html-css',
     'nestjs',
     'angular',
     'java',
@@ -43,8 +48,7 @@ export const skillNameDictionary = {
     js: 'JavaScript',
     ts: 'TypeScript',
     'C/C++': 'C/C++',
-    Html5: 'Html5',
-    Css5: 'Css5',
+    'html-css': 'Html & Css',
     nestjs: 'Nest.js',
     angular: 'Angular',
     java: 'Java',
@@ -63,19 +67,18 @@ export const skillLogoDictionary = {
     nextjs: nextjs.src,
     react: react.src,
     docker: docker.src,
-    js: null, //javascript.src,
+    js: js.src,
     ts: ts.src,
-    'C/C++': null,
-    Html5: null, //Html5.src,
-    Css5: null, //Css5.src,
+    'C/C++': cpp.src,
+    'html-css': html5.src,
     nestjs: nestjs.src,
     angular: angular.src,
     java: java.src,
     kotlin: kotlin.src,
-    python: null, //python.src,
+    python: python.src,
     sql: sql.src,
-    postgresql: null, //postgresql.src,
-    mongodb: null, //mongodb.src,
+    postgresql: postgresql.src,
+    mongodb: mongodb.src,
     git: git.src,
     sass: sass.src,
     lwjgl: lwjgl.src,
