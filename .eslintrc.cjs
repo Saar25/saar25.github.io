5e6165af26db9ea45480ed90820ea64db9482ca4
Saar Tako <saartako25@gmail.com>
2025-04-19 00:12:07 +0300

    fixed lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,15 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:@next/next/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-}
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@next/next/recommended',
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    rules: {
+        'no-empty-pattern': 'warn',
+    },
+};
